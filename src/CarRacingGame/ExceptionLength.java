d0e9b66c30b5208bd652d52cdb9adf96fc235730
kim-sung-jun <dd8873@naver.com>
2021-10-08 14:23:03 +0900

    자동차 레이싱 게임(...)

--- src/CarRacingGame/ExceptionLength.java
+++ src/CarRacingGame/ExceptionLength.java
@@ -1,7 +1,7 @@
 package CarRacingGame;
 
 public class ExceptionLength extends Exception {
-    @Override
+    @Override //바꿔야함
     public String getMessage() {
         return " 자동차 이름은 5자 이하만 가능합니다 ";
     }
