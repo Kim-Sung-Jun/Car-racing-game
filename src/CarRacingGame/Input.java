5cd9eb4c9a0aade0ab3a434ceac352e09fdfeef5
kim-sung-jun <dd8873@naver.com>
2021-10-12 22:52:11 +0900

    자동차 레이싱 게임(...)

--- src/CarRacingGame/Input.java
+++ src/CarRacingGame/Input.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 public class Input {
 
-    private final Scanner scanner = new Scanner(System.in);
+    private static final Scanner scanner = new Scanner(System.in);
 
     public String inputCarName() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
